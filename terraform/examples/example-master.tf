9a7a014cb92fdf05e184a5abfc8bc2c7b4c317cd
Brian Fitzwater - IDI-C <brian.fitzwater@gsa.gov>
2019-09-18 12:12:01 -0400

    fix terraform validation checks

--- terraform/examples/example-master.tf
+++ terraform/examples/example-master.tf
@@ -2,7 +2,7 @@
 // list all accounts and inventory each one using the OrganizationAccessRole
 // if accounts_info = ""
 module "example_master" {
-  source        = "github.com/GSA/grace-inventory/terraform?ref=master"
+  source        = "github.com/GSA/grace-inventory/terraform"
   accounts_info = ""
   source_file   = "../../release/grace-inventory-lambda.zip"
 }
