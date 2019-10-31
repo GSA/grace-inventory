990c178de0ae9161f50140bb995bd9c74ee6bf40
Taofiq Badmus <tbadmus1975@gmail.com>
2019-10-31 00:02:13 -0400

    add secrets test

---
+++ handler/helpers/secretsmanager.go
@@ -0,0 +1,42 @@
+package helpers
+
+import (
+	"errors"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/client"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/service/secretsmanager"
+
+	"github.com/aws/aws-sdk-go/service/secretsmanager/secretsmanageriface"
+)
+
+// SecretsManagerSvc ...
+type SecretsManagerSvc struct {
+	Client secretsmanageriface.SecretsManagerAPI
+}
+
+// NewSecretsManagerSvc ...
+func NewSecretsManagerSvc(cfg client.ConfigProvider, cred *credentials.Credentials) (*SecretsManagerSvc, error) {
+	if cfg == nil {
+		return nil, errors.New("nil ConfigProvider")
+	}
+	return &SecretsManagerSvc{
+		Client: secretsmanager.New(cfg, &aws.Config{Credentials: cred}),
+	}, nil
+}
+
+// Secrets ... pages through ListSecretsPages to get list of Secrets
+func (svc SecretsManagerSvc) Secrets() ([]*secretsmanager.SecretListEntry, error) {
+	var results []*secretsmanager.SecretListEntry
+	err := svc.Client.ListSecretsPages(&secretsmanager.ListSecretsInput{},
+		func(page *secretsmanager.ListSecretsOutput, lastPage bool) bool {
+			results = append(results, page.SecretList...)
+			return !lastPage
+		})
+	if err != nil {
+		return nil, err
+	}
+	return results, nil
+}
+
