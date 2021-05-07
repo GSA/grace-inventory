160048e36a133628035ef5431d75c12d8bee398e
Tom Lynch <tom.n.lynch@gsa.gov>
2021-05-07 11:44:27 -0400

    updates after Terraform upgrade to 0.13

--- versions.tf
+++ versions.tf
@@ -1,4 +1,18 @@
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      # TF-UPGRADE-TODO
+      #
+      # No source detected for this provider. You must add a source address
+      # in the following format:
+      #
+      # source = "your-registry.example.com/organization/aws"
+      #
+      # For more information, see the provider source documentation:
+      #
+      # https://www.terraform.io/docs/configuration/providers.html#provider-source
+    }
+  }
 }
